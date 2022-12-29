0d29a6ae4cf3ef2e8119367ce11c0b69eb0b0188
Natasha Draper <natasha@draper.net.za>
2022-12-29 16:43:46 +0200

    Remove undodir

--- lua/natashz/sets.lua
+++ lua/natashz/sets.lua
@@ -3,7 +3,6 @@ vim.opt.hlsearch = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.wrap = false
-vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
