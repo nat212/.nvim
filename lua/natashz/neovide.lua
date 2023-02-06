5fa5766c7d8f1e4a5334c7c55b6d31a158e0af34
Natasha Draper <natasha.draper@entelect.co.za>
2023-02-06 13:10:10 +0200

    Add neovide configs

---
+++ lua/natashz/neovide.lua
@@ -0,0 +1,7 @@
+if vim.g.neovide then
+	vim.opt.guifont = { "Fira Code Nerd Font", "h14" }
+	vim.g.neovide_transparency = 0.8
+	vim.g.transparency = 0.8
+	vim.g.neovide_floating_blur_amount_x = 2.0
+	vim.g.neovide_floating_blur_amount_y = 2.0
+end
