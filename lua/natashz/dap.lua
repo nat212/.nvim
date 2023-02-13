783fd8f30d0b0f9ce39b8977b136b194a9986e6e
Natasha Draper <natasha.draper@entelect.co.za>
2023-02-13 10:06:49 +0200

    Debug + toggleterm

--- lua/natashz/dap.lua
+++ lua/natashz/dap.lua
@@ -1,79 +1,71 @@
 local M = {}
 local keymap = require("natashz.keymap")
 
+M.ui_open = false
+
 M.start_debugging = function()
-	require("dap").continue()
-	require("dapui").open({ reset = true })
+  require("dap").continue()
+  require("dapui").open({ reset = true })
+  M.ui_open = true
 end
 
 M.setup = function()
-	local dapui = require("dapui")
-	local dap = require("dap")
-	dapui.setup({
-		layouts = {
-			{
-				elements = {
-					{
-						id = "scopes",
-						size = 0.25,
-					},
-					{
-						id = "breakpoints",
-						size = 0.25,
-					},
-					{
-						id = "stacks",
-						size = 0.25,
-					},
-					{
-						id = "watches",
-						size = 0.25,
-					},
-				},
-				position = "left",
-				size = 40,
-			},
-			{
-				elements = { {
-					id = "repl",
-					size = 1.0,
-				} },
-				position = "bottom",
-				size = 10,
-			},
-		},
-	})
+  local dapui = require("dapui")
+  local dap = require("dap")
+  dapui.setup({
+      mappings = {
+          expand = "<CR>",
+          open = "o",
+          remove = "d",
+          edit = "e",
+          repl = "r",
+      },
+      sidebar = {
+          elements = {
+              { id = "scopes",      size = 0.5 },
+              { id = "breakpoints", size = 0.25 },
+              { id = "stacks",      size = 0.25 },
+          },
+          size = 40,
+          position = "right",
+      },
+      tray = {
+          elements = { "repl", "watches" },
+          size = 10,
+          position = "bottom",
+      },
+      floating = { border = "rounded", mappings = { close = "q" } },
+  })
 
-	-- Breakpoints
-	keymap.nnoremap("<leader>b", dap.toggle_breakpoint, { silent = true })
-	keymap.nnoremap("<leader>B", dap.set_breakpoint, { silent = true })
-	keymap.nnoremap("<leader>lp", function()
-		require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-	end)
-	-- REPL
-	keymap.nnoremap("<leader>dr", function()
-		dapui.update_render({})
-	end)
-	-- Starting
-	keymap.nnoremap("<leader>do", function()
-		dapui.toggle({ reset = true })
-		dapui.update_render({})
-	end, { silent = true })
-	keymap.nnoremap("<leader>2do", function()
-		dapui.toggle({ layout = 2 })
-		dapui.update_render({})
-	end)
-	keymap.nnoremap("<leader>1do", function()
-		dapui.toggle({ layout = 1 })
-		dapui.update_render({})
-	end)
-	keymap.nnoremap("<leader>dd", M.start_debugging, { silent = true })
-	keymap.nnoremap("<leader>ds", dap.stop, { silent = true })
-	-- In debugging
-	keymap.nnoremap("<F5>", dap.continue, { silent = true })
-	keymap.nnoremap("<F10>", dap.step_over, { silent = true })
-	keymap.nnoremap("<F11>", dap.step_into, { silent = true })
-	keymap.nnoremap("<F12>", dap.step_out, { silent = true })
+  -- Breakpoints
+  keymap.nnoremap("<leader>b", dap.toggle_breakpoint, { silent = true })
+  keymap.nnoremap("<leader>B", dap.set_breakpoint, { silent = true })
+  keymap.nnoremap("<leader>lp", function()
+    require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+  end)
+  -- Starting
+  keymap.nnoremap("<leader>dq", function()
+    dapui.close()
+    M.ui_open = false
+  end)
+  keymap.nnoremap("<leader>do", function()
+    dapui.open({ reset = true })
+    M.ui_open = true
+  end, { silent = true })
+  -- keymap.nnoremap("<leader>2do", function()
+  --   dapui.toggle({ layout = 2 })
+  -- end)
+  -- keymap.nnoremap("<leader>1do", function()
+  --   dapui.toggle({ layout = 1 })
+  --   dapui.update_render({})
+  -- end)
+  keymap.nnoremap("<leader>dd", M.start_debugging, { silent = true })
+  keymap.nnoremap("<leader>ds", dap.stop, { silent = true })
+  -- In debugging
+  keymap.nnoremap("<F5>", dap.continue, { silent = true })
+  keymap.nnoremap("<F10>", dap.step_over, { silent = true })
+  keymap.nnoremap("<F11>", dap.step_into, { silent = true })
+  keymap.nnoremap("<F12>", dap.step_out, { silent = true })
 end
 
 return M
--- lua/natashz/toggleterm.lua
+++ lua/natashz/toggleterm.lua
@@ -3,28 +3,40 @@ local M = {}
 M.is_dap_open = false
 
 function _G.set_terminal_keymaps()
-	local opts = { buffer = 0 }
-	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  local opts = { buffer = 0 }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
   vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 end
 
 M.setup = function()
-	require("toggleterm").setup({
-		size = function(term)
-			if term.direction == "horizontal" then
-				return 15
-			elseif term.direction == "vertical" then
-				return vim.o.columns * 0.4
-			end
-		end,
-		open_mapping = [[<c-\>]],
-		shell = require("natashz.util").shell,
-		direction = "vertical",
-	})
+  require("toggleterm").setup({
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<c-\>]],
+      shell = require("natashz.util").shell,
+      direction = "vertical",
+      on_close = function()
+        if require("natashz.dap").ui_open then
+          require("dapui").open({ reset = true })
+        end
+      end,
+      on_open = function()
+        if require("natashz.dap").ui_open then
+          require("dapui").open({ reset = true })
+        end
+      end,
+      start_in_insert = true,
+      persist_mode = true,
+  })
 
   vim.cmd([[autocmd! TermOpen term://* lua set_terminal_keymaps()]])
 end
