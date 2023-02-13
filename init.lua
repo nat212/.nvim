4a0117b20b441d8a98336e51decd8f1f636ed83c
Natasha Draper <natasha.draper@entelect.co.za>
2023-02-13 13:08:04 +0200

    Transparent background

--- init.lua
+++ init.lua
@@ -13,3 +13,8 @@ augroup highlight_yank
     autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 40})
 augroup end
 ]])
+
+-- Transparent bg
+vim.cmd([[
+au ColorScheme * hi Normal ctermbg=none guibg=none
+]])
