8a5a7acf4e06384ac6b276a9ec715a5bb827af17
Natasha Draper <natasha.draper@entelect.co.za>
2023-02-13 09:41:55 +0200

    Python + stuff

--- lua/natashz/lsp.lua
+++ lua/natashz/lsp.lua
@@ -1,7 +1,7 @@
-local util = require "natashz.util"
+local util = require("natashz.util")
 -- Symbols keybinding
 local toggle_outline = function()
-	vim.cmd("SymbolsOutline")
+  vim.cmd("SymbolsOutline")
 end
 vim.keymap.set("n", "<C-O>", toggle_outline, { silent = true })
 
@@ -10,182 +10,189 @@ local on_attach = require("natashz.lsp_common").on_attach
 
 -- Organise python imports
 local function py_organise_imports()
-	vim.cmd("Neoformat isort")
+  vim.cmd("Neoformat isort")
 end
 
 -- Organise TS imports
 local function ts_organise_imports()
-	local params = {
-		command = "_typescript.organizeImports",
-		arguments = { vim.api.nvim_buf_get_name(0) },
-		title = "",
-	}
-	vim.lsp.buf.execute_command(params)
+  local params = {
+      command = "_typescript.organizeImports",
+      arguments = { vim.api.nvim_buf_get_name(0) },
+      title = "",
+  }
+  vim.lsp.buf.execute_command(params)
 end
 
 local lspconfig = require("lspconfig")
 
 require("mason-lspconfig").setup_handlers({
-	function(server_name) -- Default handler
-		lspconfig[server_name].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-	end,
-  ["lua_ls"] = function ()
-    -- Let neodev handle it.
-  end,
-  ["omnisharp"] = function (server_name)
-    lspconfig[server_name].setup {
-      cmd = {util.cmds.omnisharp},
-      on_attach = on_attach,
-      capabilities = capabilities,
-      enable_editorconfig_support = true,
-      enable_roslyn_analyzers = true,
-      organize_imports_on_format = true,
-      enable_import_completion = true,
-    }
-  end,
-	["powershell_es"] = function(server_name)
-		local ps_bundle
-		if require("natashz.util").is_windows then
-			ps_bundle = vim.fs.normalize("C:/src/PowerShellEditorServices")
-		end
-		lspconfig[server_name].setup({
-			bundle_path = ps_bundle,
-		})
-	end,
-	["diagnosticls"] = function(server_name)
-		lspconfig[server_name].setup({
-			filetypes = {
-				"python",
-				"typescript",
-				"javascript",
-				"html",
-				"css",
-				"scss",
-				"less",
-				"typescriptreact",
-				"javascriptreact",
-				"svelte",
-				"vue",
-			},
-			root_dir = function(fname)
-				return require("lspconfig").util.root_pattern(
-					".git",
-					"setup.cfg",
-					"pyproject.toml",
-					"tox.ini",
-					"package.json",
-					"yarn.lock",
-					"Pipfile",
-					"Pipfile.lock"
-				)(fname) or vim.fn.getcwd()
-			end,
-		})
-	end,
-	["pylsp"] = function(server_name)
-		local python3_path = vim.fs.dirname(vim.g.python3_host_prog)
-		local pylsp_path = python3_path .. "/pylsp"
-		lspconfig[server_name].setup({
-			cmd = { pylsp_path },
-			capabilities = capabilities,
-			on_attach = on_attach,
-			commands = {
-				OrganiseImports = {
-					py_organise_imports,
-					description = "Organise Imports",
-				},
-			},
-			settings = {
-				pylsp = {
-					plugins = {
-						pydocstyle = {
-							enable = true,
-							convention = "numpy",
-							addIgnore = { "D104" },
-						},
-						rope_autoimport = {
-							enabled = true,
-						},
-						flake8 = {
-							maxLineLength = 88,
-						},
-						pycodestyle = {
-							enabled = true,
-							maxLineLength = 88,
-						},
-						pyflakes = {
-							enabled = false,
-						},
-					},
-				},
-			},
-		})
-	end,
-	["jdtls"] = function()
-		-- vim.cmd([[
-		--     augroup jdtls
-		--         autocmd!
-		--         autocmd FileType java lua require'natashz.jdtls_setup'.setup()
-		--     augroup end
-		-- ]])
-		-- lspconfig['jdtls'].setup {
-		-- 	capabilities = capabilities,
-		-- 	on_attach = on_attach,
-		-- 	cmd = {
-		-- 		"java",
-		-- 		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
-		-- 		"-Dosgi.bundles.defaultStartLevel=4",
-		-- 		"-Declipse.product=org.eclipse.jdt.ls.core.product",
-		-- 		"-Dlog.protocol=true",
-		-- 		"-Dlog.level=ALL",
-		-- 		"-Xms1g",
-		-- 		"--add-modules=ALL-SYSTEM",
-		-- 		"--add-opens",
-		-- 		"java.base/java.util=ALL-UNNAMED",
-		-- 		"--add-opens",
-		-- 		"java.base/java.lang=ALL-UNNAMED",
-		-- 		"-jar",
-		-- 		"C:/tools/jdt-language-server/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
-		-- 		"-configuration",
-		-- 		"C:/tools/jdt-language-server/config_win",
-		-- 	},
-		-- 	root_dir = function(fname)
-		-- 		return require("lspconfig").util.root_pattern("pom.xml", "gradle.build", ".git")(fname) or vim.fn.getcwd()
-		-- 	end,
-		-- }
-	end, -- Use nvim-jdtls
-	["tsserver"] = function()
-		lspconfig["tsserver"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			commands = {
-				OrganiseImports = {
-					ts_organise_imports,
-					description = "Organise Imports",
-				},
-			},
-		})
-	end,
-	["angularls"] = function()
-		lspconfig["angularls"].setup({
-			on_attach = function(client, buffer)
-				client.server_capabilities.renameProvider = false
-				on_attach(client, buffer)
-			end,
-			capabilities = capabilities,
-		})
-	end,
-	["csharp_ls"] = function()
-		lspconfig["csharp_ls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			root_dir = function(fname)
-				return require("lspconfig").util.root_pattern(".git", "*.sln")(fname) or vim.fn.getcwd()
-			end,
-		})
-	end,
+    function(server_name) -- Default handler
+      lspconfig[server_name].setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+      })
+    end,
+    ["lua_ls"] = function()
+      -- Let neodev handle it.
+    end,
+    ["omnisharp"] = function(server_name)
+      lspconfig[server_name].setup({
+          cmd = { util.cmds.omnisharp },
+          on_attach = on_attach,
+          capabilities = capabilities,
+          enable_editorconfig_support = true,
+          enable_roslyn_analyzers = true,
+          organize_imports_on_format = true,
+          enable_import_completion = true,
+      })
+    end,
+    ["powershell_es"] = function(server_name)
+      local ps_bundle
+      if require("natashz.util").is_windows then
+        ps_bundle = vim.fs.normalize("C:/src/PowerShellEditorServices")
+      end
+      lspconfig[server_name].setup({
+          bundle_path = ps_bundle,
+      })
+    end,
+    ["diagnosticls"] = function(server_name)
+      -- lspconfig[server_name].setup({
+      -- 	filetypes = {
+      -- 		"python",
+      -- 		"typescript",
+      -- 		"javascript",
+      -- 		"html",
+      -- 		"css",
+      -- 		"scss",
+      -- 		"less",
+      -- 		"typescriptreact",
+      -- 		"javascriptreact",
+      -- 		"svelte",
+      -- 		"vue",
+      -- 	},
+      -- 	root_dir = function(fname)
+      -- 		return require("lspconfig").util.root_pattern(
+      -- 			".git",
+      -- 			"setup.cfg",
+      -- 			"pyproject.toml",
+      -- 			"tox.ini",
+      -- 			"package.json",
+      -- 			"yarn.lock",
+      -- 			"Pipfile",
+      -- 			"Pipfile.lock"
+      -- 		)(fname) or vim.fn.getcwd()
+      -- 	end,
+      -- })
+    end,
+    ["jedi_language_server"] = function(server_name)
+      lspconfig[server_name].setup({
+          cmd = { util.cmds.jedi },
+          capabilities = capabilities,
+          on_attach = on_attach,
+      })
+    end,
+    ["pylsp"] = function(server_name)
+      local python3_path = vim.fs.dirname(vim.g.python3_host_prog)
+      local pylsp_path = python3_path .. "/pylsp"
+      lspconfig[server_name].setup({
+          cmd = { pylsp_path },
+          capabilities = capabilities,
+          on_attach = on_attach,
+          commands = {
+              OrganiseImports = {
+                  py_organise_imports,
+                  description = "Organise Imports",
+              },
+          },
+          settings = {
+              pylsp = {
+                  plugins = {
+                      pydocstyle = {
+                          enable = true,
+                          convention = "numpy",
+                          addIgnore = { "D104" },
+                      },
+                      rope_autoimport = {
+                          enabled = true,
+                      },
+                      flake8 = {
+                          maxLineLength = 88,
+                      },
+                      pycodestyle = {
+                          enabled = true,
+                          maxLineLength = 88,
+                      },
+                      pyflakes = {
+                          enabled = false,
+                      },
+                  },
+              },
+          },
+      })
+    end,
+    ["jdtls"] = function()
+      -- vim.cmd([[
+      --     augroup jdtls
+      --         autocmd!
+      --         autocmd FileType java lua require'natashz.jdtls_setup'.setup()
+      --     augroup end
+      -- ]])
+      -- lspconfig['jdtls'].setup {
+      -- 	capabilities = capabilities,
+      -- 	on_attach = on_attach,
+      -- 	cmd = {
+      -- 		"java",
+      -- 		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+      -- 		"-Dosgi.bundles.defaultStartLevel=4",
+      -- 		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+      -- 		"-Dlog.protocol=true",
+      -- 		"-Dlog.level=ALL",
+      -- 		"-Xms1g",
+      -- 		"--add-modules=ALL-SYSTEM",
+      -- 		"--add-opens",
+      -- 		"java.base/java.util=ALL-UNNAMED",
+      -- 		"--add-opens",
+      -- 		"java.base/java.lang=ALL-UNNAMED",
+      -- 		"-jar",
+      -- 		"C:/tools/jdt-language-server/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
+      -- 		"-configuration",
+      -- 		"C:/tools/jdt-language-server/config_win",
+      -- 	},
+      -- 	root_dir = function(fname)
+      -- 		return require("lspconfig").util.root_pattern("pom.xml", "gradle.build", ".git")(fname) or vim.fn.getcwd()
+      -- 	end,
+      -- }
+    end, -- Use nvim-jdtls
+    ["tsserver"] = function()
+      lspconfig["tsserver"].setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+          commands = {
+              OrganiseImports = {
+                  ts_organise_imports,
+                  description = "Organise Imports",
+              },
+          },
+      })
+    end,
+    ["angularls"] = function()
+      lspconfig["angularls"].setup({
+          on_attach = function(client, buffer)
+            client.server_capabilities.renameProvider = false
+            on_attach(client, buffer)
+          end,
+          capabilities = capabilities,
+      })
+    end,
+    ["csharp_ls"] = function()
+      lspconfig["csharp_ls"].setup({
+          on_attach = on_attach,
+          capabilities = capabilities,
+          root_dir = function(fname)
+            return require("lspconfig").util.root_pattern(".git", "*.sln")(fname) or vim.fn.getcwd()
+          end,
+      })
+    end,
 })
 
 lspconfig.gdscript.setup({ on_attach = on_attach, capabilities = capabilities })
@@ -194,48 +201,48 @@ lspconfig.gdscript.setup({ on_attach = on_attach, capabilities = capabilities })
 local luasnip = require("luasnip")
 local cmp = require("cmp")
 if cmp == nil then
-	return
+  return
 end
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		-- { name = "buffer" },
-		{ name = "luasnip" },
-		{ name = "path" },
-	}),
+    snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-d>"] = cmp.mapping.scroll_docs( -4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable( -1) then
+            luasnip.jump( -1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        -- { name = "buffer" },
+        { name = "luasnip" },
+        { name = "path" },
+    }),
 })
 
 -- Snippets
--- lua/natashz/util.lua
+++ lua/natashz/util.lua
@@ -3,26 +3,28 @@ local M = {}
 M.is_windows = vim.loop.os_uname().sysname == "Windows_NT"
 
 M.get_init_vim = function()
-	if M.is_windows then
-		return vim.fs.normalize(vim.loop.os_homedir() .. "/AppData/Local/nvim/init.vim")
-	else
-		return vim.fs.normalize("$XDG_CONFIG_HOME/nvim/init.vim")
-	end
+  if M.is_windows then
+    return vim.fs.normalize(vim.loop.os_homedir() .. "/AppData/Local/nvim/init.vim")
+  else
+    return vim.fs.normalize("$XDG_CONFIG_HOME/nvim/init.vim")
+  end
 end
 M.mason_dir = vim.fs.normalize(vim.fn.stdpath("data") .. "/mason")
 
 M.cmds = {
-	dart_debug = vim.fs.normalize(M.mason_dir .. "/packages/dart-debug-adapter/extension/out/dist/debug.js"),
-  omnisharp = "omnisharp",
-  delve = "dlv",
+    dart_debug = vim.fs.normalize(M.mason_dir .. "/packages/dart-debug-adapter/extension/out/dist/debug.js"),
+    omnisharp = "omnisharp",
+    delve = "dlv",
+    jedi = "jedi-language-server",
 }
 
 if M.is_windows then
-	M.shell = "pwsh"
+  M.shell = "pwsh"
   M.cmds.omnisharp = "omnisharp.cmd"
   M.cmds.delve = "dlv.cmd"
+  M.cmds.jedi = "jedi-language-server.cmd"
 else
-	M.shell = nil
+  M.shell = nil
 end
 
 return M
